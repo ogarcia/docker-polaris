c1d009a98fb8bb5edbba8bcd3b076d45abb6a526
Óscar García Amor <ogarcia@connectical.com>
2025-02-05 10:39:32 +0100

    Updates Polaris to 0.15.0

--- Containerfile
+++ Containerfile
@@ -7,12 +7,10 @@ ADD https://github.com/agersant/polaris/releases/download/${POLARIS_VERSION}/Pol
 RUN /polaris/build/build.sh
 
 FROM alpine:${ALPINE_VERSION}
-RUN apk -U --no-progress upgrade && \
-    apk --no-progress add libgcc sqlite-libs && \
+RUN adduser -S -D -H -h /var/lib/polaris -s /sbin/nologin -G users -g polaris polaris && \
     install -d -m0755 -o100 -g100 /var/cache/polaris && \
     install -d -m0755 -o100 -g100 /var/lib/polaris && \
-    install -d -m0755 -o100 -g100 /var/log/polaris && \
-    rm -f /var/cache/apk/*
+    install -d -m0755 -o100 -g100 /var/log/polaris
 COPY --from=builder /polaris/pkg /
 EXPOSE 5050
 VOLUME [ "/music", "/var/cache/polaris", "/var/lib/polaris" ]
--- container/build.sh
+++ container/build.sh
@@ -10,7 +10,7 @@
 apk -U --no-progress upgrade
 
 # install build deps
-apk --no-progress add build-base curl openssl openssl-dev sqlite-dev
+apk --no-progress add build-base curl openssl openssl-dev sqlite-dev sqlite-static
 curl https://sh.rustup.rs -sSf | sh -s -- -q -y --default-toolchain stable
 
 # extract software
@@ -21,12 +21,13 @@ tar xzf polaris.tar.gz
 cd /polaris/src/polaris
 source $HOME/.cargo/env
 POLARIS_WEB_DIR="/usr/share/polaris/web" \
-  POLARIS_SWAGGER_DIR="/usr/share/polaris/swagger" \
+  POLARIS_CONFIG_DIR="/var/lib/polaris" \
+  POLARIS_DATA_DIR="/var/lib/polaris" \
   POLARIS_DB_DIR="/var/lib/polaris" \
   POLARIS_LOG_DIR="/var/log/polaris" \
   POLARIS_CACHE_DIR="/var/cache/polaris" \
   POLARIS_PID_DIR="/tmp/polaris" \
-  RUSTFLAGS="-C target-feature=-crt-static" cargo build --release
+  cargo build --release
 
 # test binary
 /polaris/src/polaris/target/release/polaris --help
@@ -38,13 +39,4 @@ install -D -m0755 "/polaris/src/polaris/target/release/polaris" \
   "/polaris/pkg/bin/polaris"
 install -d -m0755 "/polaris/pkg/usr/share/polaris"
 cp -r "web" "/polaris/pkg/usr/share/polaris"
-cp -r "swagger" "/polaris/pkg/usr/share/polaris"
 find "/polaris/pkg/usr/share/polaris" -type f -exec chmod -x {} \;
-
-# create polaris user
-adduser -S -D -H -h /var/lib/polaris -s /sbin/nologin -G users \
-  -g polaris polaris
-install -d -m0755 "/polaris/pkg/etc"
-install -m644 "/etc/passwd" "/polaris/pkg/etc/passwd"
-install -m644 "/etc/group" "/polaris/pkg/etc/group"
-install -m640 -gshadow "/etc/shadow" "/polaris/pkg/etc/shadow"
